1e6d73f5d8ea545fffc2ddf4d24ea4ad05aaaab6
Mahmoud <mohmoud14567@gmail.com>
2023-07-11 21:00:45 +0300

    7/11

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('monthly_working_hours')->default(160);
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
